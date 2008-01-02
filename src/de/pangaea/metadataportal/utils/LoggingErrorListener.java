3a483db3f2868ddc0632e2254855a888dd79bddf
Uwe Schindler <uschindler@pangaea.de>
2008-01-02 21:46:07 +0000

    bump year

--- src/de/pangaea/metadataportal/utils/LoggingErrorListener.java
+++ src/de/pangaea/metadataportal/utils/LoggingErrorListener.java
@@ -1,5 +1,5 @@
 /*
- *   Copyright 2007 panFMP Developers Team c/o Uwe Schindler
+ *   Copyright 2007-2008 panFMP Developers Team c/o Uwe Schindler
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
