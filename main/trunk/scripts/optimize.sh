b28f672dcc2ff177592af0b12cca4609c46e5315
thetaphi <thetaphi@2a318abd-a832-0410-b4be-d2e6e36bb944>
2008-09-05 17:52:27 +0000

    - add new script with class for checking indexes - cleanup .sh files

--- main/trunk/scripts/optimize.sh
+++ main/trunk/scripts/optimize.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd `dirname $0`
 source ./classpath.sh
-java -Xms64M -Xmx512M -Dlog4j.configuration=file:./default.log.properties de.pangaea.metadataportal.harvester.Optimizer "./config.xml" $1
+java -Xms64M -Xmx512M -Dlog4j.configuration=file:./default.log.properties de.pangaea.metadataportal.harvester.Optimizer "./config.xml" "$@"
