d238c666a527b5678c3d0bd755898e4058cbc256
thetaphi <thetaphi@2a318abd-a832-0410-b4be-d2e6e36bb944>
2013-04-15 17:13:13 +0000

    Nuke old trunk branch

--- main/trunk/scripts/classpath.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-CLASSPATH=
-for i in ../dist/*.jar ../libs/core/*.jar ../libs/optional/*.jar; do
-	CLASSPATH="$CLASSPATH:$i"
-done
-if [ -x /usr/bin/cygpath ]; then
-	CLASSPATH=`cygpath -pw "$CLASSPATH"`
-fi
-export CLASSPATH
