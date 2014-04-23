bc15f262dfe1795d5ee182b01e92a91f91a27814
thetaphi <thetaphi@2a318abd-a832-0410-b4be-d2e6e36bb944>
2014-04-23 18:36:31 +0000

    Remove old scripts, update other ones (preliminary, does not yet work)

--- main/trunk/scripts/classpath.sh
+++ main/trunk/scripts/classpath.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 CLASSPATH=
-for i in ../dist/*.jar ../libs/core/*.jar ../libs/optional/*.jar; do
+for i in ../dist/*.jar; do
 	CLASSPATH="$CLASSPATH:$i"
 done
 if [ -x /usr/bin/cygpath ]; then
