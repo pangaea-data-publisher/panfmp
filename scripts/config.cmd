ceeeec340ee0d08639cffb3b5a0d06d3f6647eac
Uwe Schindler <uschindler@pangaea.de>
2014-04-23 18:36:31 +0000

    Remove old scripts, update other ones (preliminary, does not yet work)

--- scripts/config.cmd
+++ scripts/config.cmd
@@ -10,9 +10,3 @@ SET PANFMP_TOOLS_JAVA_OPTIONS=-Xms64M -Xmx512M
 
 REM log4j configuration file for harvesting and management tools
 SET PANFMP_TOOLS_LOG4J_CONFIG="./console.log.properties"
-
-REM java options for jetty webserver (if installed)
-SET PANFMP_JETTY_JAVA_OPTIONS=-Xms128M -Xmx1024M -Djetty.port=8801 -Djetty.host=127.0.0.1
-
-REM log4j configuration file for jetty webserver
-SET PANFMP_JETTY_LOG4J_CONFIG="./console.log.properties"
