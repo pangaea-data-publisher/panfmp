e0a24ae8f00ca9e1898d78591f0167deb043986b
thetaphi <thetaphi@2a318abd-a832-0410-b4be-d2e6e36bb944>
2014-06-16 11:30:08 +0000

    efactor config, move Elasticsearch stuff out of TargetIndexConfig

--- main/trunk/src/de/pangaea/metadataportal/processor/ElasticsearchConnection.java
+++ main/trunk/src/de/pangaea/metadataportal/processor/ElasticsearchConnection.java
@@ -18,25 +18,44 @@ package de.pangaea.metadataportal.processor;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
+import org.elasticsearch.action.admin.indices.alias.Alias;
+import org.elasticsearch.action.admin.indices.alias.IndicesAliasesRequestBuilder;
+import org.elasticsearch.action.admin.indices.alias.IndicesAliasesResponse;
+import org.elasticsearch.action.admin.indices.create.CreateIndexRequestBuilder;
+import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexResponse;
+import org.elasticsearch.action.admin.indices.mapping.put.PutMappingResponse;
+import org.elasticsearch.action.support.IndicesOptions;
 import org.elasticsearch.client.Client;
+import org.elasticsearch.client.IndicesAdminClient;
 import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.collect.ImmutableMap;
 import org.elasticsearch.common.settings.ImmutableSettings;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.indices.IndexAlreadyExistsException;
 
 import de.pangaea.metadataportal.config.Config;
 import de.pangaea.metadataportal.config.HarvesterConfig;
+import de.pangaea.metadataportal.config.TargetIndexConfig;
 
 /**
  * TODO
  * 
  * @author Uwe Schindler
  */
-public class ElasticsearchConnection implements Closeable {
+public final class ElasticsearchConnection implements Closeable {
   private static final org.apache.commons.logging.Log log = org.apache.commons.logging.LogFactory.getLog(ElasticsearchConnection.class);
   
   private Client client;
+  private Config conf;
 
   public static final int ELASTICSEARCH_DEFAULT_PORT = 9300;
 
@@ -46,6 +65,7 @@ public class ElasticsearchConnection implements Closeable {
     if (log.isDebugEnabled()) {
       log.debug("ES connection settings: " + settings.getAsMap());
     }
+    this.conf = config;
     this.client = new TransportClient(settings, false)
       .addTransportAddresses(config.esTransports.toArray(new TransportAddress[config.esTransports.size()]));
   }
@@ -57,14 +77,189 @@ public class ElasticsearchConnection implements Closeable {
     log.info("Closed connection to Elasticsearch.");
   }
   
-  public Client client() {
+  private void checkOpen() {
     if (client == null)
       throw new IllegalStateException("Elasticsearch TransportClient is already closed.");
+  }
+  
+  public Client client() {
+    checkOpen();
     return client;
   }
   
   public DocumentProcessor getDocumentProcessor(HarvesterConfig iconfig, String targetIndex) throws IOException {
     return new DocumentProcessor(client(), iconfig, targetIndex);
+  }
+  
+  private static final String HARVESTER_METADATA_MAPPING;
+  static {
+    try {
+      HARVESTER_METADATA_MAPPING = XContentFactory.jsonBuilder().startObject()
+        .startObject("_source")
+          .field("enabled", true)
+        .endObject()
+        .startObject("_all")
+          .field("enabled", false)
+        .endObject()
+        .startArray("dynamic_templates")
+          .startObject()
+            .startObject("kv_pairs")
+              .field("match", "*")
+              .startObject("mapping")
+                .field("type", "string").field("index", "no").field("store", false)
+              .endObject()
+            .endObject()
+          .endObject()
+        .endArray()
+      .endObject().string();
+    } catch (IOException ioe) {
+      throw new Error(ioe);
+    }
+  }
+  
+  private void addFieldMappings(Map<String, Object> map) throws IOException {
+    if (map.containsKey(conf.fieldnameDatestamp) || map.containsKey(conf.fieldnameSource) || map.containsKey(conf.fieldnameXML)) {
+      throw new IllegalArgumentException("The given mapping is not allowed to contain properties for internal field: " +
+          Arrays.asList(conf.fieldnameDatestamp, conf.fieldnameSource, conf.fieldnameXML));
+    }
+    map.put(conf.fieldnameDatestamp, ImmutableMap.of(
+      "type", "date",
+      "format", "dateOptionalTime",
+      "precision_step", 8,
+      "include_in_all", false
+    ));
+    map.put(conf.fieldnameSource, ImmutableMap.of(
+      "type", "string",
+      "index", "not_analyzed",
+      "include_in_all", false,
+      "store", false
+    ));
+    map.put(conf.fieldnameXML, ImmutableMap.of(
+      "type", "string",
+      "index", "no",
+      "include_in_all", false,
+      "store", false
+    ));
+  }
+  
+  @SuppressWarnings("unchecked")
+  private Map<String,Object> getMapping() throws IOException {
+    Map<String,Object> mapping, props;
+    if (conf.esMapping != null) {
+      mapping = XContentFactory.xContent(XContentType.JSON).createParser(conf.esMapping).mapOrderedAndClose();
+      if (mapping.containsKey(conf.typeName)) {
+        if (mapping.size() != 1) {
+          throw new IllegalArgumentException("If the typeName is part of the mapping, it must be the single root element.");
+        }
+        mapping = (Map<String,Object>) mapping.get(conf.typeName);
+      }
+      props = (Map<String,Object>) mapping.get("properties");
+      if (props == null) {
+        mapping.put("properties", props = new LinkedHashMap<>());
+      }
+    } else {
+      mapping = new LinkedHashMap<>();
+      mapping.put("properties", props = new LinkedHashMap<>());
+    }
+    addFieldMappings(props);
+    return mapping;
+  }
+  
+  /** Creates the index (if needed), configures it (mapping), and creates aliases. The real index name to be used is returned. */
+  public String createIndex(TargetIndexConfig ticonf, boolean rebuilder) throws IOException {
+    checkOpen();
+    
+    final IndicesAdminClient indicesAdmin = client.admin().indices();
+    
+    log.info("Getting index name for alias='" + ticonf.indexName + "'...");
+    final String realIndexName;
+    final Iterator<String> indexes = indicesAdmin.prepareGetAliases(ticonf.indexName).get().getAliases().keysIt();
+    if (indexes.hasNext()) {
+      String found = indexes.next();
+      log.info("Alias exists and points to index='" + found + "'.");
+      if (indexes.hasNext()) {
+        throw new IOException("There are more than one index referred by alias='"+ticonf.indexName+"'");
+      }
+      if (rebuilder) {
+        realIndexName = ticonf.indexName + (found.equals(ticonf.indexName + ticonf.nameSuffix1) ? ticonf.nameSuffix2 : ticonf.nameSuffix1);
+        log.info("As rebuilding is requested, we create a new index: " + realIndexName);
+        final DeleteIndexResponse resp = indicesAdmin.prepareDelete(realIndexName).setIndicesOptions(IndicesOptions.lenientExpandOpen()).get();
+        log.info("Pre-existing index deleted: " + resp.isAcknowledged());
+      } else {
+        realIndexName = found;
+      }
+    } else {
+      log.info("Alias does not yet exist, start fresh...");
+      realIndexName = ticonf.indexName + ticonf.nameSuffix1;
+    }
+
+    log.info("Creating index='" + realIndexName + "'...");
+    try {
+      final CreateIndexRequestBuilder req = indicesAdmin.prepareCreate(realIndexName)
+        .setCause(rebuilder ? "for rebuilding" : "new harvesting")
+        .addMapping(DocumentProcessor.HARVESTER_METADATA_TYPE, HARVESTER_METADATA_MAPPING)
+        .addMapping(conf.typeName, getMapping());
+      if (ticonf.indexSettings != null) req.setSettings(ticonf.indexSettings);
+      if (!rebuilder) req.addAlias(new Alias(ticonf.indexName));
+      final CreateIndexResponse resp = req.get();
+      log.info("Index and mappings created: " + resp.isAcknowledged());
+    } catch (IndexAlreadyExistsException e) {
+      log.info("Index already exists. Updating mappings for index='" + realIndexName + "'...");
+      {
+        final PutMappingResponse resp = indicesAdmin.preparePutMapping(realIndexName)
+            .setType(DocumentProcessor.HARVESTER_METADATA_TYPE)
+            .setSource(HARVESTER_METADATA_MAPPING)
+            .setIgnoreConflicts(false)
+            .get();
+        log.info("Harvester metadata mapping updated: " + resp.isAcknowledged());
+      }
+      {
+        final PutMappingResponse resp = indicesAdmin.preparePutMapping(realIndexName)
+            .setType(conf.typeName)
+            .setSource(getMapping())
+            .setIgnoreConflicts(false)
+            .get();
+        log.info("XML metadata mapping updated: " + resp.isAcknowledged());
+      }
+    }
+
+    return realIndexName;
+  }
+  
+  /** Closes the index after harvesting and update the aliases to point to the active index. */
+  public void closeIndex(TargetIndexConfig ticonf, String realIndexName, boolean cleanShutdown) throws IOException {
+    checkOpen();
+    
+    log.info("Flushing data...");
+    client.admin().indices().prepareFlush(realIndexName).get();
+    
+    final IndicesAdminClient indicesAdmin = client.admin().indices();
+    
+    final Iterator<String> indexes = indicesAdmin.prepareGetAliases(ticonf.indexName).get().getAliases().keysIt();
+    String aliasedIndex = null;
+    if (indexes.hasNext()) {
+      aliasedIndex = indexes.next();
+      if (indexes.hasNext()) {
+        throw new IOException("There are more than one index referred by alias='" + ticonf.indexName + "'");
+      }
+    }
+    
+    if (cleanShutdown && !realIndexName.equals(aliasedIndex)) {
+      log.info("Redirecting alias '" + ticonf.indexName + "' to new index: " + realIndexName);
+      {
+        IndicesAliasesRequestBuilder req = indicesAdmin.prepareAliases();
+        if (aliasedIndex != null) {
+          req.removeAlias(aliasedIndex, ticonf.indexName);
+        }
+        IndicesAliasesResponse resp = req.addAlias(realIndexName, ticonf.indexName).get();
+        log.info("Aliases redirected: " + resp.isAcknowledged());
+      }
+      if (aliasedIndex != null) {
+        log.info("Deleting orphaned index: " + aliasedIndex);
+        DeleteIndexResponse resp = indicesAdmin.prepareDelete(aliasedIndex).get();
+        log.info("Index deleted: " + resp.isAcknowledged());
+      }
+    }
   }
   
 }
