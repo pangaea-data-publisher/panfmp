e343a699ae7d2ce809c7842c71432eab235a26d9
thetaphi <thetaphi@2a318abd-a832-0410-b4be-d2e6e36bb944>
2017-06-29 15:24:42 +0000

    Fix attribute handling in XML to JSON converter, disable attribute handling in JSON type by default

--- main/trunk/src/de/pangaea/metadataportal/processor/MetadataDocument.java
+++ main/trunk/src/de/pangaea/metadataportal/processor/MetadataDocument.java
@@ -346,7 +346,7 @@ public class MetadataDocument {
                   continue;
                 // we need to do this, otherwise may get adjacent text nodes (e.g. for XSL docfrags):
                 nod.normalize();
-                final Object o = new XMLToKeyValuePairs(true).convertChilds(nod);
+                final Object o = new XMLToKeyValuePairs(false).convertChilds(nod);
                 if (o != null) {
                   if (log.isTraceEnabled()) log.trace("AddField: " + f.name + '=' + o);
                   kv.add(f.name, o);
