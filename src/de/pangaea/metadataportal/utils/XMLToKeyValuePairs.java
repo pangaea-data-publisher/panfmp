277ff3e2797eeeada5b1fd4adef05802bca8efb0
Uwe Schindler <uwe@thetaphi.de>
2022-03-31 12:26:54 +0200

    Use jakarta.xml.bind instead of javax (for Java 11 compatibility)

--- src/de/pangaea/metadataportal/utils/XMLToKeyValuePairs.java
+++ src/de/pangaea/metadataportal/utils/XMLToKeyValuePairs.java
@@ -24,10 +24,10 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import javax.xml.XMLConstants;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBElement;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBElement;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Unmarshaller;
 
 import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
